e22caf94415cf3fe40d5b280d8a0c5946af5287a
Alex <alex.m.tang@gmail.com>
2019-12-13 12:01:03 -0800

    Add dark mode poc

--- colors.js
+++ colors.js
@@ -4,4 +4,15 @@ const COLORS = {
   green: "#61a58b"
 };
 
+export const DARK_MODE_COLORS = {
+  ...COLORS,
+  white: "#000000",
+  black: "#ffffff",
+  green: "#61a58b"
+};
+
 export default COLORS;
+
+// usage:
+// import COLORS from 'colors';
+// import { DARK_MODE_COLORS } from 'colors';
